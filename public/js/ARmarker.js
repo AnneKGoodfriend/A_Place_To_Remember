cbac244c2655ac9c4afd0570f7f5839b5680660e
itp student <itpstudent@172-16-222-147.DYNAPOOL.NYU.EDU>
2017-04-20 19:13:16 -0400

    camera turned

--- public/js/ARmarker.js
+++ public/js/ARmarker.js
@@ -70,13 +70,14 @@ window.ARThreeOnLoad = function() {
 
 		var renderer = new THREE.WebGLRenderer({antialias: true});
 		if (arController.orientation === 'portrait') {
-			var w = (window.innerWidth / arController.videoHeight) * arController.videoWidth;
-			var h = window.innerWidth;
+			// var w = (window.innerWidth / arController.videoHeight) * arController.videoWidth;
+			// var h = window.innerWidth;
 			renderer.setSize(300, 300);
 			// renderer.domElement.style.paddingBottom = (w-h) + 'px';
 		} else {
 			if (/Android|mobile|iPad|iPhone/i.test(navigator.userAgent)) {
-				renderer.setSize(window.innerWidth, (window.innerWidth / arController.videoWidth) * arController.videoHeight);
+				// renderer.setSize(window.innerWidth, (window.innerWidth / arController.videoWidth) * arController.videoHeight);
+				renderer.setSize(300, 300);
 			} else {
 				renderer.setSize(arController.videoWidth, arController.videoHeight);
 				// document.body.className += ' desktop';
