4ace99ce9c971b8c548f8bc199334651ff8274b9
itp student <itpstudent@172-16-222-147.DYNAPOOL.NYU.EDU>
2017-04-20 20:03:43 -0400

    camera

--- public/js/ARmarker.js
+++ public/js/ARmarker.js
@@ -57,12 +57,40 @@ function pauseAudio() {
    sometrack.pause(); 
 } 
 
+if (window.ARController && ARController.getUserMediaThreeScene) {
+  ARThreeOnLoad()
+}
 
-window.ARThreeOnLoad = function() {
-	ARController.getUserMediaThreeScene({maxARVideoSize: 320, cameraParam: 'Data/camera_para-iPhone 5 rear 640x480 1.0m.dat', 
-	onSuccess: function(arScene, arController, arCamera) {
+function ARThreeOnLoad() {
+  navigator
+    .mediaDevices
+    .enumerateDevices()
+    .then(function(devices) {
+      var device = devices.find(function(element) {
+        return element.label.indexOf('back') !== -1
+      })
 
-		var canvasHolder = $('#canvasHolder');
+      var videoParams = {deviceId: device ? {exact: device.deviceId} : undefined}
+
+      cameraSuccess(videoParams);
+    })
+    .catch(function(err) {
+      alert(err.name + ": " + err.message);
+    })
+}
+
+function cameraSuccess(videoParams) {
+  ARController.getUserMediaThreeScene({
+    maxARVideoSize: 640,
+    cameraParam:    'camera_para.dat',
+    deviceId:       videoParams.deviceId,
+    onSuccess:      createAR
+  })
+}
+
+
+function createAR(){
+			var canvasHolder = $('#canvasHolder');
 
 		document.body.className = arController.orientation;
 
@@ -158,12 +186,20 @@ window.ARThreeOnLoad = function() {
 
 		tick();
 
-	}});
-
-	delete window.ARThreeOnLoad;
-
-};
-
-if (window.ARController && ARController.getUserMediaThreeScene) {
-	ARThreeOnLoad();
+		delete window.ARThreeOnLoad;
 }
+
+// window.ARThreeOnLoad = function() {
+// 	ARController.getUserMediaThreeScene({maxARVideoSize: 320, cameraParam: 'Data/camera_para-iPhone 5 rear 640x480 1.0m.dat', 
+// 	onSuccess: function(arScene, arController, arCamera) {
+
+// 		//blah
+// 	}
+
+
+
+// });
+
+// 	delete window.ARThreeOnLoad;
+
+// };
