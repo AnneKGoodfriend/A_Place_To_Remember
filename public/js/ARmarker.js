63e422e542f303940abbb864ae0c140d6782c4be
itp student <itpstudent@172-16-222-147.DYNAPOOL.NYU.EDU>
2017-04-21 00:06:24 -0400

    camera

--- public/js/ARmarker.js
+++ public/js/ARmarker.js
@@ -86,7 +86,7 @@ function cameraSuccess(videoParams) {
 
   ARController.getUserMediaThreeScene({
     maxARVideoSize: 640,
-    cameraParam:    'Data/camera_para.dat',
+    // cameraParam:    'Data/camera_para.dat',
     facingMode: 	"environment",
     deviceId:       "ef450d668f40b22fd9eceb449a9084c5f209e0bbe6f7992353f5f0b99c0f152c",
     onSuccess:      createAR
