95c2549e625d31a3a9c443f7f3474b3d70e7b2bb
itp student <itpstudent@172-16-219-45.DYNAPOOL.NYU.EDU>
2017-04-26 21:19:53 -0400

    test

--- public/js/artoolkit.api.js
+++ public/js/artoolkit.api.js
@@ -1078,38 +1078,40 @@ console.log(videoParams)
 
 ARController.getUserMedia = function(configuration) {
 
-	//SET THIS UP TO HAPPEN BEFORE THE FUNCTION??????
-	// navigator
-	//     .mediaDevices
-	//     .enumerateDevices()
-	//     .then(function(devices) {
-	//       var device = devices.find(function(element) {
-	//       	console.log(element)
-
-	//       		if(element.label.indexOf("back") != -1){// == "camera2 0, facing back"){
-	//       			// console.log(element);
-
-	//       	 		      videoParams = {
-	// 					      	// deviceId: element.deviceId
-
-	// 					      	deviceId: "ef450d668f40b22fd9eceb449a9084c5f209e0bbe6f7992353f5f0b99c0f152c"
-	// 					   }
-
-	//       		}
-	//       		console.log(videoParams.deviceId);
-	//       		console.log('videoParams:')
-	// 			console.log(videoParams)
-
-	//       	})
-	      	
-	//         // return element.label.indexOf('back') !== -1
-	//       })
-
-
-		// var facing = configuration.facingMode || 'environment';
-
 		var onSuccess = configuration.onSuccess;
 		var onError = configuration.onError || function(err) { console.error("ARController.getUserMedia", err); };
+
+	//SET THIS UP TO HAPPEN BEFORE THE FUNCTION??????
+	navigator
+	    .mediaDevices
+	    .enumerateDevices()
+	    .then(function(devices) {
+	      var device = devices.find(function(element) {
+	      	console.log(element)
+
+	      		if(element.label.indexOf("back") != -1){// == "camera2 0, facing back"){
+	      			// console.log(element);
+
+	      	 		      videoParams = {
+						      	// deviceId: element.deviceId
+
+						      	deviceId: "ef450d668f40b22fd9eceb449a9084c5f209e0bbe6f7992353f5f0b99c0f152c"
+						   }
+
+	      		}
+	      		console.log(videoParams.deviceId);
+	      		console.log('videoParams:')
+				console.log(videoParams)
+
+	      	})
+	      	
+	        // return element.label.indexOf('back') !== -1
+	      })
+
+
+		var facing = configuration.facingMode || 'environment';
+
+	
 
 		var video = document.createElement('video');
 
@@ -1178,8 +1180,8 @@ ARController.getUserMedia = function(configuration) {
 
 		//mediaDevicesConstraints.facingMode = facing;
 
-		// mediaDevicesConstraints = { audio: false, video: { mandatory: {sourceId: videoParams.deviceId}}};
-		mediaDevicesConstraints = { audio: false, video: { mandatory: {sourceId: "ef450d668f40b22fd9eceb449a9084c5f209e0bbe6f7992353f5f0b99c0f152c"}}};
+		mediaDevicesConstraints = { audio: false, video: { mandatory: {sourceId: videoParams.deviceId}}};
+		// mediaDevicesConstraints = { audio: false, video: { mandatory: {sourceId: "ef450d668f40b22fd9eceb449a9084c5f209e0bbe6f7992353f5f0b99c0f152c"}}};
 		//mediaDevicesConstraints = { audio: false, video: { optional: [{facingMode: 'environment'}]}};
 
 		// mediaDevicesConstraints.video.optional = [];
