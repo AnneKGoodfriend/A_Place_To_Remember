442565294eb823b31f684e67b1516cd78993b76f
itp student <itpstudent@172-16-216-168.DYNAPOOL.NYU.EDU>
2017-04-27 23:17:04 -0400

    map position

--- public/js/ARmarker.js
+++ public/js/ARmarker.js
@@ -171,8 +171,8 @@ function createAR(arScene, arController, arCameraParam) {
 	// material.needsUpdate = true;
 	 // var material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
 	var plane = new THREE.Mesh( geometry, material );
-	plane.position.x = 3.52;
-	plane.position.y = .01;
+	plane.position.x = 3.5;
+	plane.position.y = 0;
 	//something.position.(an axis) == a property!!!!!
 	//something.position.set(x, y, z)a function which takes a glorious VECTOR 3!!!!!!!!!!!!!!!
 
